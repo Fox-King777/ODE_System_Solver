f70fb91dec887e2a2a0b746151be02a58f1cf673
Fox-King777 <zheyu.ll.chen@gmail.com>
2024-05-07 15:10:01 -0700

    updated plotting tools to include scatter toggle

--- plotting_tools.py
+++ plotting_tools.py
@@ -15,6 +15,8 @@ def plot_ode(
     trial_solution,
     analytical_solution: Callable,
     t: np.array,
+    scatter_trial=False,
+    scatter_analytical=False,
 ):
     """Plots the trial solution of the neural network against the analytical solution for ordinary differential equations.
 
@@ -27,12 +29,21 @@ def plot_ode(
         None
     """
     res = trial_solution(t, neural_network, neural_network.weights)
+    an_sol = analytical_solution(t)
 
     sns.set_theme(style="darkgrid", palette="muted", font="DeJavu Serif")
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot()
-    ax.plot(t, res, lw=1)
-    ax.plot(t, analytical_solution(t), lw=1)
+
+    if scatter_trial:
+        ax.scatter(t, res, lw=1)
+    else:
+        ax.plot(t, res, lw=1)
+    if scatter_analytical:
+        ax.scatter(t, an_sol, lw=1)
+    else:
+        ax.plot(t, an_sol, lw=1)
+
     sns.despine()
     ax.set_xlabel("t")
     ax.set_ylabel(r"$\Psi$")
@@ -44,6 +55,8 @@ def plot_system_ode(
     trial_solution,
     analytical_solution: Callable,
     t: np.array,
+    scatter_trial=False,
+    scatter_analytical=False,
 ):
     """Plots the trial solution of the neural network against the analytical solution for system of two ordinary differential equations.
 
@@ -55,31 +68,61 @@ def plot_system_ode(
     Returns:
         None
     """
-    res = [
-        trial_solution(t, neural_networks[i], neural_networks[i].weights, i)
-        for i in range(len(neural_networks))
-    ]
+    res = np.array(
+        [
+            trial_solution(t, neural_networks[i], neural_networks[i].weights, i)
+            for i in range(len(neural_networks))
+        ]
+    )
+    an_sol = analytical_solution(t)
 
     sns.set_theme(style="darkgrid", palette="muted", font="DeJavu Serif")
+
     if len(res) == 2:
         fig = plt.figure(figsize=(10, 10))
         ax = fig.add_subplot()
-        ax.plot(res[0], res[1], lw=1)
-        ax.plot(analytical_solution(t)[0], analytical_solution(t)[1], lw=1)
+
+        if scatter_trial:
+            ax.scatter(res[0], res[1], lw=1)
+        else:
+            ax.plot(res[0], res[1], lw=1)
+        if scatter_analytical:
+            ax.scatter(an_sol[0], an_sol[1], lw=1)
+        else:
+            ax.plot(an_sol[0], an_sol[1], lw=1)
+
         ax.set_xlabel(r"$\Psi_1$")
         ax.set_ylabel(r"$\Psi_2$")
+
     if len(res) == 3:
         fig = plt.figure(figsize=(10, 10), projection="3d")
         ax = fig.add_subplot()
-        ax.plot(res[0], res[1], res[2], lw=1)
-        ax.plot(
-            analytical_solution(t)[0],
-            analytical_solution(t)[1],
-            analytical_solution[2],
-            lw=1,
-        )
+
+        if scatter_trial:
+            ax.scatter(res[0], res[1], res[2], lw=1)
+        else:
+            ax.plot(res[0], res[1], res[2], lw=1)
+        if scatter_analytical:
+            ax.scatter(an_sol[0], an_sol[1], an_sol[2], lw=1)
+        else:
+            ax.plot(an_sol[0], an_sol[1], an_sol[2], lw=1)
+
         ax.set_xlabel(r"$\Psi_1$")
         ax.set_ylabel(r"$\Psi_2$")
         ax.set_zlabel(r"$\Psi_3$")
+
     sns.despine()
     plt.legend(["MLP", "Analytical"])
+
+
+def print_error(res: np.ndarray, an_sol: np.ndarray):
+    """Print difference between trial solution and analytical solution.
+
+    Args:
+        res: trial solution
+        an_sol: analytical solution
+
+    Returns:
+        None
+    """
+    print(res - an_sol)
